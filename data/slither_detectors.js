d8c40c1f61a2114a0debff8ec9579b8a5b380081
choco-cupcake <98981813+choco-cupcake@users.noreply.github.com>
2023-03-30 11:44:44 +0100

    webui moved - solc 0.8.19 - new custom detector
    solc auto downloader todo

--- data/slither_detectors.js
+++ data/slither_detectors.js
@@ -3,6 +3,6 @@ const detectors_slither_high = ["arbitrary-send-erc20","incorrect-shift","shadow
 
 const detectrs_slither_badcode = ["tautology", "write-after-write", "divide-before-multiply", "shadowing-local", "incorrect-unary", "assert-state-change", "boolean-equal", "erc20-indexed", "redundant-statements", "dead-code", "unused-state", "immutable-states", "too-many-digits", "constable-states"]
 
-const custom_detectors = []
+const custom_detectors = ["unprotected-write"]
 
 module.exports = {custom_detectors, detectors_slither_high, detectrs_slither_badcode}
